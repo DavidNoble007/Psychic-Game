33f8bec6eab304433a5ce6fc70f4dbf2c58b5f87
DavidNoble <david.r.noble@outlook.com>
2019-01-15 20:57:44 -0700

    file structure

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+
+
+// creates variables for use later as wins and losses
+// var wins = 0;
+// var losses = 0;
+
+
+ // Create variables that hold references to the places in the HTML where we want to display things.
+var wins = document.getElementById("wins");
+var losses = document.getElementById("losses");
+var guessesleft = document.getElementById("guessesleft");
+var sofar = document.getElementById("sofar");
